db9b6a69e1dc4419ccc18c9e665feb985d488aaf
Andrew Ottaviano <Andrew Ottaviano>
2017-04-08 19:56:42 -0500

    Add project files.

---
+++ SpeedDial/ProcessIcons.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using SpeedDial.Properties;
+
+namespace SpeedDial
+{
+    class ProcessIcons : IDisposable
+    {
+        NotifyIcon ni;
+
+        public ProcessIcons()
+        {
+            ni = new NotifyIcon();
+
+            //Convert the Bitmap to an icon and use for icon picture
+            //Credit: Muhammad Ali Didar's answer at http://stackoverflow.com/questions/8174393/convert-bitmap-to-icon
+            Bitmap bit = Resources.Icon;
+            IntPtr ptr = bit.GetHicon();
+            ni.Icon = Icon.FromHandle(ptr);
+            ni.Text = "SpeedDial";
+            ni.MouseDoubleClick += Ni_MouseDoubleClick;
+        }
+
+        private void Ni_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Settings s = new Settings();
+            s.Show();
+        }
+
+        public void Display()
+        {
+            ni.Visible = true;
+        }
+
+        public void Dispose()
+        {
+            ni.Visible = false;
+        }
+    }
+}
---
+++ SpeedDial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SpeedDial
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            using (ProcessIcons pi = new ProcessIcons())
+            {
+                pi.Display();
+
+                Application.Run();
+            }
+
+        }
+    }
+}
